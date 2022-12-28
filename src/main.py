453bdb2e15fdccc473665dc3f8cd140bc87bc7b9
Danny Chen <dannychen8102@gmail.com>
2022-12-28 16:40:11 -0500

    Fixed buggy CSV writing

--- src/main.py
+++ src/main.py
@@ -22,37 +22,100 @@ minBalance = 0
 maxBalance = 0
 endingBalance = 0
 
-for index, row in transactions.iterrows():
-    customer_id = row['customerID']
-    date = formatter.convert_date(row['date'])
-    amount = row['amount']
+for i, transactions_row in transactions.iterrows():
+    t_customer_id = transactions_row['customerID']
+    t_date = formatter.convert_date(transactions_row['date'])
+    t_amount = transactions_row['amount']
 
     transaction_data = {
-        "customerID": customer_id,
-        "MM/YYYY": date,
+        "customerID": t_customer_id,
+        "MM/YYYY": t_date,
         "minBalance": minBalance,
         "maxBalance": maxBalance,
         "endingBalance": endingBalance
     }
 
-    # Case 1: CustomerID already exists
-    if (customer_id in accounts['customerID'].values):
-        # print("Case 1")
-        selected_row = accounts[accounts['customerID'] == customer_id]
-        # Case 1a: CustomerID and MM/YYYY already exists, only need to update balances
-        if date in selected_row['MM/YYYY'].values:
-            queried_row = accounts.query('customerID == @customer_id & `MM/YYYY` == @date')
-            print(queried_row)
-
-            bal.set_end(accounts, queried_row, amount)
-
-            # print("Case 1a", '\n')
-        else: # Case 1b: CustomerID exists but the MM/YYYY doesn't, need to write new entry for it
-            accounts.loc[index] = transaction_data
-            accounts.to_csv(OUTPUT_PATH, index=False)
-            # print("Case 1b", '\n')
-    else: # Case 2: CustomerID does not exist, therefore need to write new entry for it
-        bal.set_all(transaction_data, amount)
-        accounts.loc[index] = transaction_data
+    # Edge case: CSV is empty, write the first entry into CSV
+    if accounts.empty: 
+        bal.set_all(transaction_data, t_amount)
+        accounts.loc[i] = transaction_data
         accounts.to_csv(OUTPUT_PATH, index=False)
-        print("Case 2", '\n')
+        # print("Edge case occured")
+    else:
+        customerID_query = accounts.query('customerID == @t_customer_id')
+
+        # Case 0: If the customerID_query returns an empty dataframe, then customerID doesn't exist yet.
+        if customerID_query.empty:
+            accounts.loc[i] = transaction_data
+        else: 
+            date_query = accounts.query('customerID == @t_customer_id & `MM/YYYY` == @t_date')
+
+            # Case 0a: If the customer_query is able to return a dataframe, requery with the addition of the date.
+
+            if date_query.empty:
+                bal.set_all(transaction_data, t_amount)
+                accounts.loc[i] = transaction_data
+                accounts.to_csv(OUTPUT_PATH, index=False)
+                # print("Case 0a occured")
+            else:
+                date_query['endingBalance'] = bal.set_end(date_query, t_amount)
+                date_query['minBalance'] = bal.set_min(date_query)
+                date_query['maxBalance'] = bal.set_max(date_query)
+
+                index = accounts.index[(accounts['customerID'] == t_customer_id) & (accounts['MM/YYYY'] == t_date)]
+
+                accounts.loc[index] = date_query
+                accounts.to_csv(OUTPUT_PATH, index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+    # for j, accounts_row in accounts.iterrows():
+    #     print("hello")
+    #     a_customer_id = accounts_row['customerID']
+    #     a_date = formatter.convert_date(accounts_row['date'])
+
+    #     if (t_customer_id == a_customer_id):
+    #         if (t_date == a_date):
+    #             accounts_row['endingBalance'] = bal.set_end(accounts_row, t_amount)
+    #             accounts_row['minBalance'] = bal.set_min(accounts_row)
+    #             accounts_row['maxBalance'] = bal.set_max(accounts_row)
+    #             accounts.to_csv(OUTPUT_PATH, index=False)
+    #         else:
+    #             accounts.loc[j] = transaction_data
+    #             accounts.to_csv(OUTPUT_PATH, index=False)
+    #     else:
+    #         bal.set_all(transaction_data, t_amount)
+    #         accounts.to_csv(OUTPUT_PATH, index=False)
+        
+    
+
+    # # Case 1: CustomerID already exists
+    # if (customer_id in accounts['customerID'].values):
+    #     # print("Case 1")
+    #     selected_row = accounts[accounts['customerID'] == customer_id]
+    #     # Case 1a: CustomerID and MM/YYYY already exists, only need to update balances
+    #     if date in selected_row['MM/YYYY'].values:
+    #         queried_row = accounts.query('customerID == @customer_id & `MM/YYYY` == @date')
+    #         print(queried_row)
+
+    #         bal.set_end(accounts, queried_row, amount)
+
+    #         # print("Case 1a", '\n')
+    #     else: # Case 1b: CustomerID exists but the MM/YYYY doesn't, need to write new entry for it
+    #         accounts.loc[index] = transaction_data
+    #         accounts.to_csv(OUTPUT_PATH, index=False)
+    #         # print("Case 1b", '\n')
+    # else: # Case 2: CustomerID does not exist, therefore need to write new entry for it
+    #     bal.set_all(transaction_data, amount)
+    #     accounts.loc[index] = transaction_data
+    #     accounts.to_csv(OUTPUT_PATH, index=False)
+    #     print("Case 2", '\n')
--- src/set_balance.py
+++ src/set_balance.py
@@ -4,26 +4,34 @@ def set_all(transaction_data, amount):
     transaction_data['endingBalance'] = amount
 
 
-def set_end(accounts, queried_row, amount):
-    customer_id = queried_row.iat[0, 0] # Get queried row's ID
-    date = queried_row.iat[0, 1]  # Get  queried row's MM/YYYY
+def set_end(queried_row, amount):
     ending_balance = queried_row.iat[0, 4]  # Get current ending balance
-    print(customer_id)
-    print(date)
-    print(ending_balance)
 
     if amount >= 0:  # Credit transaction
         ending_balance += amount
     else:  # Debit transaction
         ending_balance -= amount
 
-    accounts.loc[(accounts['customerID'] == customer_id) & (accounts['MM/YYYY'] == date)] = ending_balance
+    return ending_balance
 
 
-def set_min(accounts, queried_row, amount):
-    current_min = queried_row['minBalance']
-    queried_row['minBalance'] = amount
+def set_min(queried_row):
+    current_min = queried_row.iat[0, 2]  # Get current minBalance
+    current_end = queried_row.iat[0, 4]  # Get current endingBalance
+
+    if current_min > current_end:
+        current_min = current_end
+        return current_min
+    else:
+        return current_min
 
 
-def set_max(accounts, queried_row, amount):
-    queried_row['maxBalance'] = amount
+def set_max(queried_row):
+    current_max = queried_row.iat[0, 3]  # Get current minBalance
+    current_end = queried_row.iat[0, 4] # Get current endingBalance
+
+    if current_max < current_end:
+        current_max = current_end
+        return current_max
+    else:
+        return current_max
